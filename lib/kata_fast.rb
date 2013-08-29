3c1c83979196357d3f79485a147051acfd9778e9
Cosimo Ranieri <co.ranieri@gmail.com>
2013-08-29 13:37:15 +0200

    cleanups

--- lib/kata_fast.rb
+++ lib/kata_fast.rb
@@ -32,33 +32,33 @@ class KataFast
     results = []
 
     six_letter_words.each do |word|
-      part_1 = word[0 .. 0]
-      part_2 = word[1 .. 5]
-      if @five_letter_words.has_key? part_2 and @one_letter_words.has_key? part_1
+      part_1 = word[0..0]
+      part_2 = word[1..5]
+      if @five_letter_words.has_key?(part_2) && @one_letter_words.has_key?(part_1)
         results << "#{part_1} + #{part_2} => #{word}\n"
       end
 
-      part_1 = word[0 .. 1]
-      part_2 = word[2 .. 5]
-      if @four_letter_words.has_key? part_2 and @two_letter_words.has_key? part_1
+      part_1 = word[0..1]
+      part_2 = word[2..5]
+      if @four_letter_words.has_key?(part_2) && @two_letter_words.has_key?(part_1)
         results << "#{part_1} + #{part_2} => #{word}\n"
       end
 
-      part_1 = word[0 .. 2]
-      part_2 = word[3 .. 5]
-      if @three_letter_words.has_key? part_1 and @three_letter_words.has_key? part_2
+      part_1 = word[0..2]
+      part_2 = word[3..5]
+      if @three_letter_words.has_key?(part_1) && @three_letter_words.has_key?(part_2)
         results << "#{part_1} + #{part_2} => #{word}\n"
       end
 
-      part_1 = word[0 .. 3]
-      part_2 = word[4 .. 5]
-      if @four_letter_words.has_key? part_1 and @two_letter_words.has_key? part_2
+      part_1 = word[0..3]
+      part_2 = word[4..5]
+      if @four_letter_words.has_key?(part_1) && @two_letter_words.has_key?(part_2)
         results << "#{part_1} + #{part_2} => #{word}\n"
       end
 
-      part_1 = word[0 .. 4]
-      part_2 = word[5 .. 5]
-      if @five_letter_words.has_key? part_1 and @one_letter_words.has_key? part_2
+      part_1 = word[0..4]
+      part_2 = word[5..5]
+      if @five_letter_words.has_key?(part_1) && @one_letter_words.has_key?(part_2)
         results << "#{part_1} + #{part_2} => #{word}\n"
       end
     end
